5ab70b11eb29dc75c4162dae97856097f3b49e05
tantreesss <tristanjosh12@gmail.com>
2026-01-29 22:21:18 +0800

    R3

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -60,10 +60,14 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ className, ...props }) => (
+        IconLeft: (
+          { className, ...props }: React.ComponentProps<typeof ChevronLeft>,
+        ) => (
           <ChevronLeft className={cn("size-4", className)} {...props} />
         ),
-        IconRight: ({ className, ...props }) => (
+        IconRight: (
+          { className, ...props }: React.ComponentProps<typeof ChevronRight>,
+        ) => (
           <ChevronRight className={cn("size-4", className)} {...props} />
         ),
       }}
